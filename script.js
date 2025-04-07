47b1b9735313624e85d55a16c0426515aa54af88
AissatouCoulibaly <coulibalyaissatou37@gmail.com>
2025-04-07 14:22:16 +0000

    first-commit

---
+++ script.js
@@ -0,0 +1,16 @@
+function retourAcceuil() {
+  window.location.href = "index.html"; // redirige vers accueil
+}
+function voirCV() {
+  window.location.href = "cv.html";
+}
+function changerTheme() {
+  document.body.classList.toggle("alt-theme");
+}
+function telechargerPDF() {
+  const bouton = document.getElementById("btn");
+  bouton.style.display = "none";
+  const element = document.getElementById("convertirEnPDF");
+  html2pdf().from(element).save("Aïssatou_Coulibaly_CV.pdf");
+  bouton.style.display = "visible";
+}
